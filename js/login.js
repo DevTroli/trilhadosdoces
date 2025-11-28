c625c9697e5c71d06f5f6549e0fb25e6b35a9df4
Troli <brawlixo123@gmail.com>
2025-11-28 13:04:07 -0300

    ♻️ refactor(login) implement native alert for feedback

--- js/login.js
+++ js/login.js
@@ -1,30 +1,28 @@
 const EMAIL_1 = "brawlixo123@gmail.com";
 const SENHA_1 = "1291392522026";
-
 const EMAIL_2 = "Yohan@Ruiz";
 const SENHA_2 = "1291392522007";
 
 const form = document.getElementById("formLogin");
-const erro = document.getElementById("loginErro");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-
+  
   const email = document.getElementById("email").value.trim();
   const senha = document.getElementById("senha").value.trim();
-
+  
   if (!email || !senha) {
-    erro.textContent = "Preencha email e senha para continuar.";
+    alert("⚠️ Preencha email e senha para continuar.");
     return;
   }
-
+  
   const loginValido =
     (email === EMAIL_1 && senha === SENHA_1) ||
     (email === EMAIL_2 && senha === SENHA_2);
-
+  
   if (loginValido) {
     window.location.href = "pedido.html";
   } else {
-    erro.textContent = "Dados inválidos. Verifique usuário e senha.";
+    alert("❌ Dados inválidos. Verifique usuário e senha.");
   }
 });
