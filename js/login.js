ea1f1e7cfca0c5b4e377e8f61f681b0ace1c2df3
Troli <brawlixo123@gmail.com>
2025-11-28 09:10:40 -0300

    ♻️ (refactor)  code to improve clarity and organizationq

--- js/login.js
+++ js/login.js
@@ -1,10 +1,13 @@
-const EMAIL_VALIDO_1 = "brawlixo123@gmail.com";
-const SENHA_VALIDA_1 = "1291392522026";
+const EMAIL_1 = "brawlixo123@gmail.com";
+const SENHA_1 = "1291392522026";
+
+const EMAIL_2 = "Yohan@Ruiz";
+const SENHA_2 = "1291392522007";
 
 const form = document.getElementById("formLogin");
 const erro = document.getElementById("loginErro");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", function(event) {
   event.preventDefault();
 
   const email = document.getElementById("email").value.trim();
@@ -15,10 +18,13 @@ form.addEventListener("submit", function (event) {
     return;
   }
 
-  if (email === EMAIL_VALIDO_1 && senha === SENHA_VALIDA_1) {
+  const loginValido =
+    (email === EMAIL_1 && senha === SENHA_1) ||
+    (email === EMAIL_2 && senha === SENHA_2);
+
+  if (loginValido) {
     window.location.href = "pedido.html";
   } else {
     erro.textContent = "Dados inválidos. Verifique usuário e senha.";
   }
 });
-
